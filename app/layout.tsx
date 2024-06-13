6d108697724d8d9f44eac76d1d5242167b3a4c0a
lewpar <lewis.o.parmenter@gmail.com>
2024-06-14 09:58:01 +1000

    Moved authorized menu items to its own row.

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,12 +34,25 @@ export default async function RootLayout({
                         <span className="text-xl">Cyber Bilby</span>
                     </a>
                     <ul>
-                        <a href="/" className="flex flex-row gap-1 items-center justify-center"><i className="ph ph-house"></i><li>Home</li></a>
-                        <a href="/blog" className="flex flex-row gap-1 items-center justify-center"><i className="ph ph-newspaper"></i><li>Blog</li></a>
-                        {
-                            isLoggedIn ? <a href="/blog/create" className="flex flex-row gap-1 items-center justify-center"><i className="ph ph-note-pencil"></i><li>Create Post</li></a> : ''
-                        }
+                        <a href="/" className="flex flex-row gap-1 items-center justify-center">
+                            <i className="ph ph-house"></i>
+                            <li>Home</li>
+                        </a>
+                        <a href="/blog" className="flex flex-row gap-1 items-center justify-center">
+                            <i className="ph ph-newspaper"></i>
+                            <li>Blog</li>
+                        </a>
                     </ul>
+                    {
+                        isLoggedIn ? 
+                        <ul>
+                            <a href="/blog/create" className="flex flex-row gap-1 items-center justify-center">
+                                <i className="ph ph-note-pencil"></i>
+                                <li>Create Post</li>
+                            </a>
+                        </ul> : ''
+                            
+                    }
                 </header>
                 <main>
                 {children}
