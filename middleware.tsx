5ad78fc80ca8efde338762b8ff39d95e9067463c
lewpar <lewis.o.parmenter@gmail.com>
2024-06-14 10:12:20 +1000

    Reverted middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,16 +1,1 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { auth } from './auth';
- 
-export async function middleware(req: NextRequest) {
-    let session = await auth();
-    let path = req.nextUrl.basePath;
-
-    if(path.startsWith('/blog/create')) {
-        if(session == null) {
-            return NextResponse.rewrite(new URL('/not-allowed', req.url), { status: 401 })
-        }
-    }
-
-    return NextResponse.next()
-}
+export { auth as middleware } from "@/auth"
