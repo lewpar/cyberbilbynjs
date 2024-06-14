22352e8c5701a4306f964c4b60eda0548263f93b
lewpar <lewis.o.parmenter@gmail.com>
2024-06-14 10:57:43 +1000

    Forgot to include blog types.

---
+++ database/models/BlogTypes.tsx
@@ -0,0 +1,15 @@
+export type BlogPost = {
+    author: string;
+    slug: string;
+    title: string;
+    content: string;
+    date: Date;
+}
+
+export type BlogShortPost = {
+    author: string;
+    slug: string;
+    title: string;
+    content: string;
+    date: Date;
+}
