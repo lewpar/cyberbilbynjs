ddaadfa930ebce88ad6648beed06382e9250e775
lewpar <lewis.o.parmenter@gmail.com>
2024-06-15 17:06:27 +1000

    Moved menu items to actions component.

--- src/app/components/auth/AdminSidebar/ActionSidebar.tsx
+++ src/app/components/auth/AdminSidebar/ActionSidebar.tsx
@@ -1,14 +1,11 @@
 import UserDetails from "../UserDetails";
-import ActionButton from "./ActionButton";
-import ActionHeader from "./ActionHeader";
+import Actions from "./Actions";
 
 export default function ActionSidebar() {
     return (
-        <div className="flex flex-col pb-4 text-white bg-slate-800 rounded-sm">
+        <div className={`flex flex-col text-white bg-slate-800 rounded-sm`}>
             <UserDetails/>
-            <ActionButton href="/author" text="Dashboard" icon="ph ph-browsers"/>
-            <ActionHeader text="Blog"/>
-            <ActionButton href="/author/create" text="Create Post" icon="ph ph-note-pencil"/>
+            <Actions/>
         </div>
     );
 }
