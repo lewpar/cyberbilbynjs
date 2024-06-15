53f70d3832d0c75db6912fed990be4d16d390e4b
lewpar <lewis.o.parmenter@gmail.com>
2024-06-15 18:42:19 +1000

    Removed padding from admin side panel.

--- src/app/components/auth/AdminPage.tsx
+++ src/app/components/auth/AdminPage.tsx
@@ -2,7 +2,7 @@ import ActionSidebar from "./AdminSidebar/ActionSidebar";
 
 export default function AdminPage({ children, }: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <div className="flex-1 flex flex-col tablet:flex-row p-4">
+        <div className="flex-1 flex flex-col tablet:flex-row">
             <ActionSidebar/>
             <div className="flex-1 p-4">
                 {children}
--- src/app/components/auth/AdminSidebar/ActionSidebar.tsx
+++ src/app/components/auth/AdminSidebar/ActionSidebar.tsx
@@ -3,7 +3,7 @@ import Actions from "./Actions";
 
 export default function ActionSidebar() {
     return (
-        <div className={`flex flex-col text-white bg-slate-800 rounded-sm`}>
+        <div className={`flex flex-col text-white bg-slate-800 border-r border-slate-900`}>
             <UserDetails/>
             <Actions/>
         </div>
