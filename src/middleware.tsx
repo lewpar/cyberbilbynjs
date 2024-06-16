d58d39c921ebabf8f7a64cc43c218f480fefa3a8
lewpar <lewis.o.parmenter@gmail.com>
2024-06-17 00:30:48 +1000

    Cleaned up middleware and authorize api route.

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const routes = [
+const protectedRoutes = [
     "/api/blog/post",
     "/author",
     "/author/create",
@@ -10,13 +10,16 @@ const routes = [
 export async function middleware(request: NextRequest) {
     let pathName = request.nextUrl.pathname;
 
-    if(!routes.includes(pathName)) {
+    if(!protectedRoutes.includes(pathName)) {
         return NextResponse.next();
     }
 
+
+    // Send a GET request to an internal API to check the user is authorized to access protected resources.
     try {
         let session = request.cookies.get("authjs.session-token");
         if(!session) {
+            // APIs should return a message instead of redirecting the user to login.
             if(pathName.startsWith("/api")) {
                 return NextResponse.json({
                     message: "You are not authorized to access this resource."
@@ -34,16 +37,19 @@ export async function middleware(request: NextRequest) {
             }
         }
 
-        let result = await fetch(`${process.env.API_BASE_URL}/api/auth/current`, {
+        // Forward the users session cookie to the authorize API to check the users roles.
+        // This is done in the API because the middleware runs in the EDGE runtime which doesnt allow Prisma client.
+        let result = await fetch(`${process.env.API_BASE_URL}/api/authorize`, {
             method: 'GET',
             headers: {
                 Cookie: `${session.name}=${session.value}`
             }
         });
 
+        // If the response from the API is not ok then the user is not authorized to access this resource.
         if(!result.ok) {
             return NextResponse.json({
-                message: "You are not authorized to access this resource."
+                message: "An internal error occured while trying to access a protected resource."
             }, 
             { status: 401 });
         }
