71ebc4dd983bcbbbefa109f34b8297da24b3680f
lewpar <lewis.o.parmenter@gmail.com>
2024-06-22 18:52:14 +1000

    Fixed function call.

--- client/src/hooks/useAuth.tsx
+++ client/src/hooks/useAuth.tsx
@@ -15,7 +15,7 @@ const useAuth = () => {
     };
 
     const login = () => {
-        setUser(getUserAccess);
+        setUser(getUserAccess());
     };
     
     return [user, logout, login] as const;
